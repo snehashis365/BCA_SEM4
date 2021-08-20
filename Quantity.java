348756fa84bd220a871bb04e9527919ba20ce5cf
snehashis365 <snehashis.2000@gmail.com>
2021-08-20 14:56:57 +0530

    Fixed

--- Quantity.java
+++ Quantity.java
@@ -1,15 +1,48 @@
-public class Quantity {
+import java.util.*;
+
+interface Discolor{
+    public void discolor(String name);
+}
+
+interface DisPrice{
+    int unitPrice=200;
+    public void disPrice();
+}
+
+public class Quantity{
     int x;
-    interface Discolor {
-        void discolor();
+    Quantity(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Qty: ");
+        x=sc.nextInt();
     }
-    interface DisPrice {
-        void disPrice();
-    }
-    void setQuantity(int x) {
-        this.x = x;
-    }
-    int getQuantity() {
+    public int getQuantity(){
         return x;
+    }
+}
+
+class PriceColor extends Quantity implements DisPrice,Discolor{
+
+    Scanner sc=new Scanner(System.in);
+    String color;
+
+    public void discolor(String name){
+        System.out.println(name);
+    }
+
+    public void disPrice(){
+        System.out.println(super.getQuantity()*unitPrice);
+    }
+
+    String getColor(){
+        System.out.print("Color: ");
+        color=sc.nextLine();
+        return color;
+    }
+
+    public static void main(String[] args) {
+        PriceColor priceColor=new PriceColor();
+        priceColor.discolor(priceColor.getColor());
+        priceColor.disPrice();
     }
 }
